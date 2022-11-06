520653c504ad4c8f0d602e8ae334b7cba3fc71b7
Kostya Kokhanov <knuckostya@gmail.com>
2022-11-06 18:16:57 +0200

    HW#4COMMIT

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,10 +17,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        + (disabled ? ' ' + s.disabled : '')
-       +(xType === 'red'  ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : '') //записать
-        + (className ? ' ' + className : '')
+    const finalClassName = s.button + " "
+        + (disabled ? s.disabled : '')
+        + (xType === "default" ? s.default
+            : xType === 'red' ? s.red
+                : xType === 'secondary' ? s.secondary : '')
+
 
     return (
         <button
