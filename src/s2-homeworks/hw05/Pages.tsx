f5ea0b1f08ecfafe3b5294f832e5960677481c82
Kostya Kokhanov <knuckostya@gmail.com>
2022-11-26 14:44:15 +0200

    hw55

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,7 +18,7 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path = '/*'></Route>
+                <Route path = '/'></Route>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}></Route>
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}></Route>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path = {'/'} element={<Error404/>}></Route>
+                <Route path = {'/*'} element={<Error404/>}></Route>
             </Routes>
         </div>
     )
